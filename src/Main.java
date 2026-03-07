1614331f3a85aa81bbcffc94ab0c776052f8c53f
AhmedG720 <a.m.t.mandili@gmail.com>
2026-03-08 00:17:30 +0300

    better buttons

--- src/Main.java
+++ src/Main.java
@@ -19,38 +19,15 @@ public class Main {
         buttonsContainer.setLayout(new GridLayout(4, 4));
         window.add(buttonsContainer, BorderLayout.SOUTH);
 
-        buttonLayout b1 = new buttonLayout("one");
-        buttonsContainer.add(b1);
-        buttonLayout b2 = new buttonLayout("one");
-        buttonsContainer.add(b2);
-        buttonLayout b3 = new buttonLayout("one");
-        buttonsContainer.add(b3);
-        buttonLayout b4 = new buttonLayout("one");
-        buttonsContainer.add(b4);
-        buttonLayout b5 = new buttonLayout("one");
-        buttonsContainer.add(b5);
-        buttonLayout b6 = new buttonLayout("one");
-        buttonsContainer.add(b6);
-        buttonLayout b7 = new buttonLayout("one");
-        buttonsContainer.add(b7);
-        buttonLayout b8 = new buttonLayout("one");
-        buttonsContainer.add(b8);
-        buttonLayout b9 = new buttonLayout("one");
-        buttonsContainer.add(b9);
-        buttonLayout b10 = new buttonLayout("one");
-        buttonsContainer.add(b10);
-        buttonLayout b11 = new buttonLayout("one");
-        buttonsContainer.add(b11);
-        buttonLayout b12 = new buttonLayout("one");
-        buttonsContainer.add(b12);
-        buttonLayout b13 = new buttonLayout("one");
-        buttonsContainer.add(b13);
-        buttonLayout b14 = new buttonLayout("one");
-        buttonsContainer.add(b14);
-        buttonLayout b15 = new buttonLayout("one");
-        buttonsContainer.add(b15);
-        buttonLayout b16 = new buttonLayout("one");
-        buttonsContainer.add(b16);
+        String[] buttonNames = { "1", "2", "3", "4", "5", "6",
+                "7", "8", "9", "=", "+", "-", "x", "÷"
+        };
+
+        for (int i = 0; i < buttonNames.length; i++) {
+            String name = buttonNames[i];
+            buttonLayout button = new buttonLayout(name);
+            buttonsContainer.add(button);
+        }
 
         window.setVisible(true);
         screen.setVisible(true);
