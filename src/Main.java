2d23917757c9ad895d530bd030b95658626cebf0
AhmedG720 <a.m.t.mandili@gmail.com>
2026-03-08 01:11:02 +0300

    functionable for now

--- src/Main.java
+++ src/Main.java
@@ -2,37 +2,51 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 import java.awt.*;
 import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Main {
+public class Main implements ActionListener {
+    JTextField screen;
+
     public static void main(String[] args) {
         JFrame window = new JFrame("My window");
         window.setSize(720, 720);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JTextField screen = new JTextField();
-        screen.setBounds(0, 0, 400, 200);
-        screen.setText("hey");
-        screen.setEditable(false);
-        window.add(screen, BorderLayout.NORTH);
+        Main theListener = new Main();
+
+        theListener.screen = new JTextField();
+        theListener.screen.setBounds(0, 0, 400, 200);
+        theListener.screen.setText("hey");
+        theListener.screen.setEditable(false);
+        window.add(theListener.screen, BorderLayout.NORTH);
 
         JPanel buttonsContainer = new JPanel();
         buttonsContainer.setLayout(new GridLayout(4, 4));
         window.add(buttonsContainer, BorderLayout.SOUTH);
 
         String[] buttonNames = { "1", "2", "3", "4", "5", "6",
-                "7", "8", "9", "=", "+", "-", "x", "÷"
+                "7", "8", "9", "=", "+", "-", "x", "÷", "del"
         };
-
         for (int i = 0; i < buttonNames.length; i++) {
             String name = buttonNames[i];
             buttonLayout button = new buttonLayout(name);
             buttonsContainer.add(button);
+            button.addActionListener(theListener);
         }
 
         window.setVisible(true);
-        screen.setVisible(true);
+        theListener.screen.setVisible(true);
         buttonsContainer.setVisible(true);
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getActionCommand());
+
+        String button = e.getActionCommand();
+
+        screen.setText(button);
     }
 
 }
--- src/buttonLayout.java
+++ src/buttonLayout.java
@@ -1,14 +1,12 @@
 import javax.swing.JButton;
 
 import java.awt.Font;
-import java.awt.font.*;
 
 public class buttonLayout extends JButton {
     // Constructors...
     public buttonLayout(String name) {
         super(name);
-
-        this.setFont(new Font("Arial", Font.BOLD, 20));
+        this.setFont(new Font("Arial", Font.BOLD, 30));
         this.setFocusable(false);
     }
 
