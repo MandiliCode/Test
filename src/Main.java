d22225dd3efaf0c9e6c7029a36f9f2bb51bd6581
AhmedG720 <a.m.t.mandili@gmail.com>
2026-03-07 21:20:17 +0300

    calcTest

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame("My window");
-
         window.setSize(720, 720);
-
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         window.setVisible(true);
+
+        JTextField screen = new JTextField();
+        screen.setBounds(0, 0, 400, 200);
+        screen.setText("hey");
+        screen.setEditable(false);
+        window.add(screen, BorderLayout.NORTH);
+        screen.setVisible(true);
     }
 
 }
