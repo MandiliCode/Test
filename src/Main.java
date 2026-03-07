0ca4f65a29e8f992c57dc70bc8c2ff22ec4a9062
AhmedG720 <a.m.t.mandili@gmail.com>
2026-03-07 23:57:29 +0300

    buttons

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 import java.awt.*;
 import javax.swing.JPanel;
-import javax.swing.JButton;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,8 +16,41 @@ public class Main {
         window.add(screen, BorderLayout.NORTH);
 
         JPanel buttonsContainer = new JPanel();
-        buttonsContainer.setLayout(new GridLayout(2, 2));
+        buttonsContainer.setLayout(new GridLayout(4, 4));
         window.add(buttonsContainer, BorderLayout.SOUTH);
+
+        buttonLayout b1 = new buttonLayout("one");
+        buttonsContainer.add(b1);
+        buttonLayout b2 = new buttonLayout("one");
+        buttonsContainer.add(b2);
+        buttonLayout b3 = new buttonLayout("one");
+        buttonsContainer.add(b3);
+        buttonLayout b4 = new buttonLayout("one");
+        buttonsContainer.add(b4);
+        buttonLayout b5 = new buttonLayout("one");
+        buttonsContainer.add(b5);
+        buttonLayout b6 = new buttonLayout("one");
+        buttonsContainer.add(b6);
+        buttonLayout b7 = new buttonLayout("one");
+        buttonsContainer.add(b7);
+        buttonLayout b8 = new buttonLayout("one");
+        buttonsContainer.add(b8);
+        buttonLayout b9 = new buttonLayout("one");
+        buttonsContainer.add(b9);
+        buttonLayout b10 = new buttonLayout("one");
+        buttonsContainer.add(b10);
+        buttonLayout b11 = new buttonLayout("one");
+        buttonsContainer.add(b11);
+        buttonLayout b12 = new buttonLayout("one");
+        buttonsContainer.add(b12);
+        buttonLayout b13 = new buttonLayout("one");
+        buttonsContainer.add(b13);
+        buttonLayout b14 = new buttonLayout("one");
+        buttonsContainer.add(b14);
+        buttonLayout b15 = new buttonLayout("one");
+        buttonsContainer.add(b15);
+        buttonLayout b16 = new buttonLayout("one");
+        buttonsContainer.add(b16);
 
         window.setVisible(true);
         screen.setVisible(true);
--- src/buttonLayout.java
+++ src/buttonLayout.java
@@ -1,9 +1,15 @@
 import javax.swing.JButton;
 
-public class buttonLayout {
-    // Constructors...
-    public buttonLayout(int number) {
+import java.awt.Font;
+import java.awt.font.*;
 
+public class buttonLayout extends JButton {
+    // Constructors...
+    public buttonLayout(String name) {
+        super(name);
+
+        this.setFont(new Font("Arial", Font.BOLD, 20));
+        this.setFocusable(false);
     }
 
 }
