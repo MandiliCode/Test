a2ecb13b4be7241c23b191d9f69bd53a01c01c8f
AhmedG720 <a.m.t.mandili@gmail.com>
2026-03-07 21:39:36 +0300

    idk what is this

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,29 @@
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import java.awt.*;
+import javax.swing.JPanel;
+import javax.swing.JButton;
 
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame("My window");
         window.setSize(720, 720);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setVisible(true);
 
         JTextField screen = new JTextField();
         screen.setBounds(0, 0, 400, 200);
         screen.setText("hey");
         screen.setEditable(false);
         window.add(screen, BorderLayout.NORTH);
+
+        JPanel buttonsContainer = new JPanel();
+        buttonsContainer.setLayout(new GridLayout(2, 2));
+        window.add(buttonsContainer, BorderLayout.SOUTH);
+
+        window.setVisible(true);
         screen.setVisible(true);
+        buttonsContainer.setVisible(true);
+
     }
 
 }
---
+++ src/buttonLayout.java
@@ -0,0 +1,9 @@
+import javax.swing.JButton;
+
+public class buttonLayout {
+    // Constructors...
+    public buttonLayout(int number) {
+
+    }
+
+}
